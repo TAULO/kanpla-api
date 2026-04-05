8704ea50223505dc044e699503417b5e17e910b1
Thomas Taulo <taulo@live.dk>
2026-04-05 20:29:34 +0200

    add methods for retrieving this and next week's menus

--- src/Kanpla.ts
+++ src/Kanpla.ts
@@ -5,7 +5,7 @@ export interface IMenuItem {
   category: string;
   photo: string;
   menu: IMenu;
-  type: 'main' | 'vegetarian' | 'other';
+  type: "main" | "vegetarian" | "other";
 }
 
 export interface IMenu {
@@ -53,8 +53,7 @@ class Kanpla {
     return d.getTime() / 1000;
   }
 
-  async getMenusByDate(date: Date): Promise<IMenuItem[]> {
-    const frontendData = await this._kanplaAPI.getFrontendByModuleId();
+  private getMenuData(frontendData: Array<any>, date: Date) {
     return frontendData.reduce((acc: any, item: any) => {
       const timestamp = this.toUnixDay(date).toString();
       const dates = item.dates[timestamp];
@@ -65,6 +64,7 @@ class Kanpla {
         category: item.category,
         photo: item.photo,
         menu: dates?.menu as IMenu,
+        date: new Date(date.getTime()),
         type,
       });
 
@@ -72,8 +72,48 @@ class Kanpla {
     }, [] as IMenuItem[]);
   }
 
+  async getMenusByDate(date: Date): Promise<IMenuItem[]> {
+    const frontendData = await this._kanplaAPI.getFrontendByModuleId();
+    return this.getMenuData(frontendData, date);
+  }
+
   async getTodayMenu(): Promise<IMenuItem[]> {
     return await this.getMenusByDate(new Date());
+  }
+
+  async getThisWeekMenu(): Promise<IMenuItem[]> {
+    const frontendData = await this._kanplaAPI.getFrontendByModuleId();
+
+    const menus: IMenuItem[] = [];
+    const today = new Date();
+    const monday = new Date(today);
+    monday.setDate(today.getDate() - ((today.getDay() + 6) % 7));
+
+    for (let i = 0; i < 5; i++) {
+      const date = new Date(monday);
+      date.setDate(monday.getDate() + i);
+      menus.push(...(await this.getMenuData(frontendData, date)));
+    }
+
+    return menus;
+  }
+
+  async getNextWeekMenu(): Promise<IMenuItem[]> {
+    const frontendData = await this._kanplaAPI.getFrontendByModuleId();
+
+    const menus: IMenuItem[] = [];
+
+    const today = new Date();
+    const nextMonday = new Date(today);
+    nextMonday.setDate(today.getDate() - ((today.getDay() + 6) % 7) + 7);
+
+    for (let i = 0; i < 5; i++) {
+      const date = new Date(nextMonday);
+      date.setDate(nextMonday.getDate() + i);
+      menus.push(...(await this.getMenuData(frontendData, date)));
+    }
+
+    return menus;
   }
 
   async getModuleIds(): Promise<string[]> {
